4fa04ad9e810e57de328f22ef1f7dc80e82e5e7a
Chumeng <2595248810@qq.com>
2023-07-14 09:22:31 +0800

    Refactored countdown timer to include reset functionality

--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,14 @@ use std::io::{self, Write};
 use std::sync::{Arc, Mutex};
 use crossterm::event::{poll, read, Event, KeyCode};
 
-fn countdown(t: u64, count: Arc<Mutex<u64>>) {
+fn countdown(t: u64, count: Arc<Mutex<u64>>, reset: Arc<Mutex<bool>>) {
     let end_time = Instant::now() + Duration::from_secs(t * 60);
     while Instant::now() < end_time {
+        if *reset.lock().unwrap() {
+            *reset.lock().unwrap() = false;
+            *count.lock().unwrap() = 0;
+            break;
+        }
         let remaining_time = end_time.duration_since(Instant::now());
         let hours = remaining_time.as_secs() / 3600;
         let remainder = remaining_time.as_secs() % 3600;
@@ -21,18 +26,20 @@ fn countdown(t: u64, count: Arc<Mutex<u64>>) {
     io::stdout().flush().unwrap(); // Flush the output buffer
 }
 
-fn timer(count: Arc<Mutex<u64>>) {
+fn timer(count: Arc<Mutex<u64>>, reset: Arc<Mutex<bool>>) {
     loop {
-        countdown(15, Arc::clone(&count));
+        countdown(15, Arc::clone(&count), Arc::clone(&reset));
         *count.lock().unwrap() += 1;
     }
 }
 
 fn main() {
     let count = Arc::new(Mutex::new(1));
+    let reset = Arc::new(Mutex::new(false));
     let count_clone = Arc::clone(&count);
+    let reset_clone = Arc::clone(&reset);
     thread::spawn(move || {
-        timer(count_clone);
+        timer(count_clone, reset_clone);
     });
 
     loop {
@@ -40,6 +47,7 @@ fn main() {
             if let Event::Key(key_event) = read().unwrap() {
                 if key_event.code == KeyCode::Char('r') {
                     *count.lock().unwrap() = 1;
+                    *reset.lock().unwrap() = true;
                 }
             }
         }

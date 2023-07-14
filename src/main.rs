7f9d8d39084c53074c2e78dff59067210119c91e
Chumeng <2595248810@qq.com>
2023-07-14 08:20:30 +0800

    Title: Add reset functionality to the timer and update Github Action
    Description:
    - Implemented a new reset feature for the timer.
    - Created a new thread to listen for keyboard input.
    - Used Arc and Mutex to share and synchronize access to the `count` variable across threads.
    - Reset the `count` variable to 1 when the 'r' key is pressed.
    - Added dependency on the `crossterm` crate for cross-platform terminal operations.
    - Updated Github Action file `rust.yml` to support reading version numbers from `Cargo.toml`.

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
 use std::time::{Duration, Instant};
-use std::thread::sleep;
+use std::thread::{self, sleep};
 use std::io::{self, Write};
+use std::sync::{Arc, Mutex};
+use crossterm::event::{poll, read, Event, KeyCode};
 
-fn countdown(t: u64, count: u64) {
+fn countdown(t: u64, count: Arc<Mutex<u64>>) {
     let end_time = Instant::now() + Duration::from_secs(t * 60);
     while Instant::now() < end_time {
         let remaining_time = end_time.duration_since(Instant::now());
@@ -11,23 +13,35 @@ fn countdown(t: u64, count: u64) {
         let minutes = remainder / 60;
         let seconds = remainder % 60;
         let millis = remaining_time.subsec_millis();
-        print!("\rTime remaining: {:02}:{:02}:{:02}.{:03} , T{}", hours, minutes, seconds, millis, count);
+        print!("\rTime remaining: {:02}:{:02}:{:02}.{:03} , T{}", hours, minutes, seconds, millis, *count.lock().unwrap());
         io::stdout().flush().unwrap(); // Flush the output buffer
         sleep(Duration::from_millis(1));  // sleep for 1 millisecond
     }
-    print!("\rNext Round, T{}", count);
+    print!("\rNext Round, T{}", *count.lock().unwrap());
     io::stdout().flush().unwrap(); // Flush the output buffer
 }
 
-fn timer() {
-    let mut count = 1;
+fn timer(count: Arc<Mutex<u64>>) {
     loop {
-        countdown(15, count);
-        count += 1;
+        countdown(1, Arc::clone(&count));
+        *count.lock().unwrap() += 1;
     }
 }
 
 fn main() {
-    timer();
-}
+    let count = Arc::new(Mutex::new(1));
+    let count_clone = Arc::clone(&count);
+    thread::spawn(move || {
+        timer(count_clone);
+    });
 
+    loop {
+        if poll(Duration::from_millis(100)).unwrap() {
+            if let Event::Key(key_event) = read().unwrap() {
+                if key_event.code == KeyCode::Char('r') {
+                    *count.lock().unwrap() = 1;
+                }
+            }
+        }
+    }
+}

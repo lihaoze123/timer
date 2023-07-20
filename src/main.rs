e8b6938eb704fd54d89ca3c2c9bd5d523a5d1f44
Chumeng <2595248810@qq.com>
2023-07-20 17:13:01 +0800

    Refine progress bar display in TUI

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,21 @@
 use std::time::{Duration, Instant};
 use std::thread::{self, sleep};
-use std::io::{self, Write, stdout};
+use std::io::stdout;
 use std::sync::{Arc, Mutex};
 use crossterm::event::{poll, read, Event, KeyCode};
-use crossterm::terminal::{disable_raw_mode, enable_raw_mode};
+use crossterm::terminal::enable_raw_mode;
 use crossterm::execute;
 use crossterm::cursor::Hide;
 use tui::Terminal;
 use tui::backend::CrosstermBackend;
-use tui::widgets::{Block, Borders, Paragraph};
+use tui::widgets::{Block, Borders, Paragraph, Gauge};
 use tui::layout::{Layout, Constraint, Direction, Alignment};
-use tui::style::{Color, Modifier, Style};
+use tui::style::{Color, Style};
 use tui::text::{Text, Spans};
 
 fn countdown(t: u64, count: Arc<Mutex<u64>>, reset: Arc<Mutex<bool>>) {
-    let end_time = Instant::now() + Duration::from_secs(t * 60);
+    let total_time = Duration::from_secs(t * 60);
+    let end_time = Instant::now() + total_time;
     let stdout = stdout();
     let backend = CrosstermBackend::new(stdout);
     let mut terminal = Terminal::new(backend).unwrap();
@@ -37,35 +38,42 @@ fn countdown(t: u64, count: Arc<Mutex<u64>>, reset: Arc<Mutex<bool>>) {
                 .margin(2)
                 .constraints(
                     [
-                        Constraint::Percentage(100),
+                        Constraint::Percentage(90),
+                        Constraint::Length(1),
                     ].as_ref()
                 )
                 .split(f.size());
             let block = Block::default()
                 .title(Spans::from(format!(
-                    "Countdown Timer | Round: {}",
+                    " Countdown Timer | Round: {} ",
                     *count.lock().unwrap()
                 )))
-                .title_style(Style::default().fg(Color::Yellow).bg(Color::Black).add_modifier(Modifier::BOLD))
                 .borders(Borders::ALL)
                 .border_style(Style::default().fg(Color::White));
+            let elapsed_time = total_time - remaining_time;
+            let progress = elapsed_time.as_secs() as f64 / total_time.as_secs() as f64;
+            let gauge = Gauge::default()
+                .block(Block::default().borders(Borders::ALL))
+                .gauge_style(Style::default().fg(Color::Yellow))
+                .percent((progress * 100.0) as u16);
             let paragraph = Paragraph::new(Text::from(format!(
                     "Time remaining: {:02}:{:02}:{:02}.{:03}",
                     hours, minutes, seconds, millis
                 )))
                 .block(block)
                 .style(Style::default().fg(Color::White))
-                .alignment(Alignment::Center); // Set alignment to Center
+                .alignment(Alignment::Center); 
             f.render_widget(paragraph, chunks[0]);
+            f.render_widget(gauge, chunks[1]);
         }).unwrap();
-        sleep(Duration::from_millis(1));  // sleep for 1 millisecond
+        sleep(Duration::from_millis(1));  
     }
     terminal.clear().unwrap();
 }
 
 fn timer(count: Arc<Mutex<u64>>, reset: Arc<Mutex<bool>>) {
     loop {
-        countdown(15, Arc::clone(&count), Arc::clone(&reset));
+        countdown(1, Arc::clone(&count), Arc::clone(&reset));
         *count.lock().unwrap() += 1;
     }
 }
@@ -91,5 +99,4 @@ fn main() {
             }
         }
     }
-    disable_raw_mode().unwrap();
 }

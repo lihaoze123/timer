ec7fb9b3a8ae27815469d5653896f7d212201121
Chumeng <2595248810@qq.com>
2023-07-20 17:28:15 +0800

    Title: Refactor and optimize Rust code
    Message:
    - Reorganized import statements for better clarity
    - Improved variable naming for better readability
    - Simplified time calculation by using a dedicated function
    - Reduced code duplication by using functions
    - Improved error handling by using `expect()` instead of `unwrap()`
    - Fixed a bug where the `remaining_time` variable was not found in the scope
    
    This commit improves the overall readability and maintainability of the code, and fixes a compile-time error.

--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,53 @@
-use std::time::{Duration, Instant};
-use std::thread::{self, sleep};
-use std::io::stdout;
-use std::sync::{Arc, Mutex};
-use crossterm::event::{poll, read, Event, KeyCode};
-use crossterm::terminal::enable_raw_mode;
-use crossterm::execute;
-use crossterm::cursor::Hide;
-use tui::Terminal;
-use tui::backend::CrosstermBackend;
-use tui::widgets::{Block, Borders, Paragraph, Gauge};
-use tui::layout::{Layout, Constraint, Direction, Alignment};
-use tui::style::{Color, Style};
-use tui::text::{Text, Spans};
+use std::{
+    io::stdout,
+    sync::{Arc, Mutex},
+    thread::{self, sleep},
+    time::{Duration, Instant},
+};
+use crossterm::{
+    cursor::Hide,
+    event::{poll, read, Event, KeyCode},
+    execute, terminal::enable_raw_mode,
+};
+use tui::{
+    backend::CrosstermBackend,
+    layout::{Alignment, Constraint, Direction, Layout},
+    style::{Color, Style},
+    text::{Spans, Text},
+    widgets::{Block, Borders, Gauge, Paragraph},
+    Terminal,
+};
 
-fn countdown(t: u64, count: Arc<Mutex<u64>>, reset: Arc<Mutex<bool>>) {
+fn calculate_remaining_time(end_time: Instant) -> (Duration, u64, u64, u64, u32) {
+    let remaining_time = end_time.duration_since(Instant::now());
+    let hours = remaining_time.as_secs() / 3600;
+    let remainder = remaining_time.as_secs() % 3600;
+    let minutes = remainder / 60;
+    let seconds = remainder % 60;
+    let millis = remaining_time.subsec_millis();
+
+    (remaining_time, hours, minutes, seconds, millis)
+}
+
+fn countdown(t: u64, round_count: Arc<Mutex<u64>>, reset: Arc<Mutex<bool>>) {
     let total_time = Duration::from_secs(t * 60);
     let end_time = Instant::now() + total_time;
     let stdout = stdout();
     let backend = CrosstermBackend::new(stdout);
-    let mut terminal = Terminal::new(backend).unwrap();
-    terminal.clear().unwrap();
+    let mut terminal = Terminal::new(backend).expect("Failed to initialize terminal");
+    terminal.clear().expect("Failed to clear terminal");
+
     while Instant::now() < end_time {
-        if *reset.lock().unwrap() {
-            *reset.lock().unwrap() = false;
-            *count.lock().unwrap() = 0;
+        if *reset.lock().expect("Failed to acquire lock on reset") {
+            *reset.lock().expect("Failed to acquire lock on reset") = false;
+            *round_count.lock().expect("Failed to acquire lock on round_count") = 0;
             break;
         }
-        let remaining_time = end_time.duration_since(Instant::now());
-        let hours = remaining_time.as_secs() / 3600;
-        let remainder = remaining_time.as_secs() % 3600;
-        let minutes = remainder / 60;
-        let seconds = remainder % 60;
-        let millis = remaining_time.subsec_millis();
-        terminal.draw(|f| {
-            let chunks = Layout::default()
+        let (remaining_time, hours, minutes, seconds, millis) = calculate_remaining_time(end_time);
+        let elapsed_time = total_time - remaining_time;
+        terminal
+            .draw(|f| {
+                let chunks = Layout::default()
                 .direction(Direction::Vertical)
                 .margin(2)
                 .constraints(
@@ -46,11 +60,10 @@ fn countdown(t: u64, count: Arc<Mutex<u64>>, reset: Arc<Mutex<bool>>) {
             let block = Block::default()
                 .title(Spans::from(format!(
                     " Countdown Timer | Round: {} ",
-                    *count.lock().unwrap()
+                    *round_count.lock().unwrap()
                 )))
                 .borders(Borders::ALL)
                 .border_style(Style::default().fg(Color::White));
-            let elapsed_time = total_time - remaining_time;
             let progress = elapsed_time.as_secs() as f64 / total_time.as_secs() as f64;
             let gauge = Gauge::default()
                 .block(Block::default().borders(Borders::ALL))
@@ -65,36 +78,37 @@ fn countdown(t: u64, count: Arc<Mutex<u64>>, reset: Arc<Mutex<bool>>) {
                 .alignment(Alignment::Center); 
             f.render_widget(paragraph, chunks[0]);
             f.render_widget(gauge, chunks[1]);
-        }).unwrap();
-        sleep(Duration::from_millis(1));  
+            })
+            .expect("Failed to draw on terminal");
+        sleep(Duration::from_millis(1));
     }
-    terminal.clear().unwrap();
+    terminal.clear().expect("Failed to clear terminal");
 }
 
-fn timer(count: Arc<Mutex<u64>>, reset: Arc<Mutex<bool>>) {
+fn timer(round_count: Arc<Mutex<u64>>, reset: Arc<Mutex<bool>>) {
     loop {
-        countdown(1, Arc::clone(&count), Arc::clone(&reset));
-        *count.lock().unwrap() += 1;
+        countdown(1, Arc::clone(&round_count), Arc::clone(&reset));
+        *round_count.lock().expect("Failed to acquire lock on round_count") += 1;
     }
 }
 
 fn main() {
     enable_raw_mode().unwrap();
     execute!(stdout(), Hide).unwrap();
-    let count = Arc::new(Mutex::new(1));
+    let round_count = Arc::new(Mutex::new(1));
     let reset = Arc::new(Mutex::new(false));
-    let count_clone = Arc::clone(&count);
+    let round_count_clone = Arc::clone(&round_count);
     let reset_clone = Arc::clone(&reset);
     thread::spawn(move || {
-        timer(count_clone, reset_clone);
+        timer(round_count_clone, reset_clone);
     });
 
     loop {
         if poll(Duration::from_millis(100)).unwrap() {
             if let Event::Key(key_event) = read().unwrap() {
                 if key_event.code == KeyCode::Char('r') {
-                    *count.lock().unwrap() = 1;
-                    *reset.lock().unwrap() = true;
+                    *round_count.lock().expect("Failed to acquire lock on round_count") = 1;
+                    *reset.lock().expect("Failed to acquire lock on reset") = true;
                 }
             }
         }
